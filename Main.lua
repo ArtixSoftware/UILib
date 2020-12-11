945a4d78e9e4f2684421e4f57638cd3d1b01cc17
ArtixSoftware <75815567+ArtixSoftware@users.noreply.github.com>
2020-12-10 19:42:02 -0600

    added keybinds lol

--- Main.lua
+++ Main.lua
@@ -1,4 +1,3 @@
---Congrats on finding the ui lib code, use it if u want i could care less
 local lib = {}
 
 local UserInputService = game:GetService("UserInputService")
@@ -13,13 +12,14 @@ lib.CurrentlySelectedSection = nil
 lib.Sections = {}
 lib.CurrentSectionNumber = 0
 lib.Focused = false
+lib.KeyBinds = {}
 
 function lib:CreateGui(ProductTextInput)
 	local ScreenGui = Instance.new("ScreenGui")
 	local Frame = Instance.new("Frame")
 	local ProductText = Instance.new("TextLabel")
 
-	ScreenGui.Parent = game.CoreGui
+	ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui") -- game.CoreGui
 
 	Frame.Parent = ScreenGui
 	Frame.BackgroundColor3 = Color3.fromRGB(79, 79, 79)
@@ -75,6 +75,7 @@ function lib:CreateSection(SectionName)
 	Section.ButtonNumber = 0
 	Section.Toggles = {}
 	Section.Focused = false
+	Section.KeybindBusy = nil
 	
 	--[[local SectionGUI = Instance.new("Frame")
 
@@ -229,6 +230,70 @@ function lib:CreateSection(SectionName)
 		Section.CurrentObject = TextButton
 	end
 	
+	function Section:Keybind(Text, CallBack)
+		local CallBack = CallBack or function() end
+		local TextButton = Instance.new("TextButton")
+
+		TextButton.Text = Text .. " <NO KEY ASSIGNED>"
+		TextButton.Parent = lib.MainGUIInstance
+		TextButton.BorderSizePixel = 0
+		TextButton.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+		TextButton.Size = UDim2.new(1, 0, 0.05, 0)
+		TextButton.ZIndex = 12
+		TextButton.Font = Enum.Font.SourceSans
+		TextButton.TextColor3 = Color3.fromRGB(255, 255, 255)
+		TextButton.TextSize = 14.000
+		TextButton.Visible = false
+		TextButton.AutoButtonColor = false
+		if Section.CurrentObject then
+			TextButton.Position = UDim2.new(1.1, 0, (Section.CurrentObject.Position.Y.Scale + Section.CurrentObject.Size.Y.Scale), 0)
+		else
+			TextButton.Position = UDim2.new(1.1, 0, 0.05, 0)
+		end
+
+		if #Section.IObjects <= 0 then
+			Section.CurrentlySelectedObject = TextButton
+			Section.CurrentlySelectedObject.Text = "> " .. Section.CurrentlySelectedObject.Text
+			Section.ButtonNumber = 1
+		end
+
+		local ReceiveFunction = function(IsDown)
+			local Found = false
+			for i,v in pairs(lib.KeyBinds) do
+				if v[3] == TextButton then
+					v = nil
+					table.remove(lib.KeyBinds, i)
+					break
+				end
+			end
+			for i,v in pairs(Section.IObjects) do
+				if v[1] == TextButton and v[3] then
+					print(tostring(v[3]))
+					Found = true
+					TextButton.Text = "> " .. Text .. " <" .. tostring(v[3].Name) .. ">"
+					if v[3] == "LMB" then
+						TextButton.Text = "> " .. Text .. " <LMB>"
+					elseif v[3] == "RMB" then
+						TextButton.Text = "> " .. Text .. " <RMB>"
+					elseif v[3] == "MMB" then
+						TextButton.Text = "> " .. Text .. " <MMB>"
+					end
+					table.insert(lib.KeyBinds, #lib.KeyBinds+1, {v[3], CallBack, TextButton, IsDown})
+					CallBack(IsDown)
+					break
+				end
+			end
+			if not Found then
+				TextButton.Text = "> " .. Text .. " <NO KEY ASSIGNED>"
+			end
+			Section.KeybindBusy = false
+		end
+
+		table.insert(Section.Objects, #Section.Objects+1, TextButton)
+		table.insert(Section.IObjects, #Section.IObjects+1, {TextButton, ReceiveFunction, Enum.KeyCode.Unknown, "Keybind"})
+		Section.CurrentObject = TextButton
+	end
+	
 	function Section:MultipleChoice(Text, Choices, CallBack)
 		local CallBack = CallBack or function() end
 		local TextButton = Instance.new("TextButton")
@@ -307,6 +372,66 @@ function lib:CreateSection(SectionName)
 	end
 	
 	UserInputService.InputBegan:Connect(function(Input)
+		if Input.UserInputType == Enum.UserInputType.MouseButton1 then
+			if Section.KeybindBusy and Section.CurrentlySelectedObject then
+				local SectionIOBject
+				for i,v in pairs(Section.IObjects) do
+					if v[1] == Section.CurrentlySelectedObject and v[2] and v[3] then
+						SectionIOBject = v
+					end
+				end
+				if SectionIOBject[2] then
+					SectionIOBject[3] = "LMB"
+					SectionIOBject[2](true)
+				end
+			else
+				for i,v in pairs(lib.KeyBinds) do
+					if v[1] == "LMB" then
+						v[2](true)
+					end
+				end
+			end
+		end
+		if Input.UserInputType == Enum.UserInputType.MouseButton2 then
+			if Section.KeybindBusy and Section.CurrentlySelectedObject then
+				local SectionIOBject
+				for i,v in pairs(Section.IObjects) do
+					if v[1] == Section.CurrentlySelectedObject and v[2] and v[3] then
+						SectionIOBject = v
+					end
+				end
+				if SectionIOBject[2] then
+					SectionIOBject[3] = "RMB"
+					SectionIOBject[2](true)
+				end
+			else
+				for i,v in pairs(lib.KeyBinds) do
+					if v[1] == "RMB" then
+						v[2](true)
+					end
+				end
+			end
+		end
+		if Input.UserInputType == Enum.UserInputType.MouseButton3 then
+			if Section.KeybindBusy and Section.CurrentlySelectedObject then
+				local SectionIOBject
+				for i,v in pairs(Section.IObjects) do
+					if v[1] == Section.CurrentlySelectedObject and v[2] and v[3] then
+						SectionIOBject = v
+					end
+				end
+				if SectionIOBject[2] then
+					SectionIOBject[3] = "MMB"
+					SectionIOBject[2](true)
+				end
+			else
+				for i,v in pairs(lib.KeyBinds) do
+					if v[1] == "MMB" then
+						v[2](true)
+					end
+				end
+			end
+		end
 		if Input.UserInputType == Enum.UserInputType.Keyboard and lib.CurrentlySelectedSection == SectionButton and SectionButton.Visible == true and Section.CurrentlySelectedObject and lib.Focused == true and lib.MainGUIInstance.Visible == true then
 			if #Section.IObjects > 0 then
 				local SectionIOBject
@@ -329,114 +454,183 @@ function lib:CreateSection(SectionName)
 						end
 					end
 				end
-				if Section.Focused then else
-					if Input.KeyCode == Enum.KeyCode.Down or Input.KeyCode == Enum.KeyCode.LeftBracket then
-						if Section.ButtonNumber+1 <= #Section.IObjects then
-							if Section.CurrentlySelectedObject then
-								if Section.CurrentlySelectedObject:IsA("TextButton") then
-									local Text = Section.CurrentlySelectedObject.Text
-									Section.CurrentlySelectedObject.Text = Text:gsub("> ", "")
+				if Section.KeybindBusy and SectionIOBject[2] then
+					SectionIOBject[3] = Input.KeyCode
+					SectionIOBject[2](true)
+				else
+					if Section.Focused then else
+						if Input.KeyCode == Enum.KeyCode.Down or Input.KeyCode == Enum.KeyCode.LeftBracket then
+							if Section.ButtonNumber+1 <= #Section.IObjects then
+								if Section.CurrentlySelectedObject then
+									if Section.CurrentlySelectedObject:IsA("TextButton") then
+										local Text = Section.CurrentlySelectedObject.Text
+										Section.CurrentlySelectedObject.Text = Text:gsub("> ", "")
+									end
 								end
-							end
-							Section.ButtonNumber += 1
-							Section.CurrentlySelectedObject = Section.IObjects[Section.ButtonNumber][1]
-							if Section.CurrentlySelectedObject then
-								if Section.CurrentlySelectedObject:IsA("TextButton") then
-									Section.CurrentlySelectedObject.Text = "> " .. Section.CurrentlySelectedObject.Text
-								end
-							end
-						end
-					end
-					if Input.KeyCode == Enum.KeyCode.Up or Input.KeyCode == Enum.KeyCode.RightBracket then
-						if Section.ButtonNumber-1 > 0 then
-							if Section.CurrentlySelectedObject then
-								if Section.CurrentlySelectedObject:IsA("TextButton") then
-									local Text = Section.CurrentlySelectedObject.Text
-									Section.CurrentlySelectedObject.Text = Text:gsub("> ", "")
-								end
-							end
-							Section.ButtonNumber -= 1
-							Section.CurrentlySelectedObject = Section.IObjects[Section.ButtonNumber][1]
-							if Section.CurrentlySelectedObject then
-								if Section.CurrentlySelectedObject:IsA("TextButton") then
-									Section.CurrentlySelectedObject.Text = "> " .. Section.CurrentlySelectedObject.Text
-								end
-							end
-						end
-					end
-				end
-				if Input.KeyCode == Enum.KeyCode.Return then
-					if Section.CurrentlySelectedObject then
-						if Section.Focused then
-							for i,v in pairs(Section.IObjects) do
-								if v == Section.CurrentlySelectedObject then
-									v[2]()
-								end
-							end
-							Section.Focused = false
-						else
-							local IsToggle = false
-							for i,v in pairs(Section.Toggles) do
-								if v[1] == Section.CurrentlySelectedObject then
-									if v[2] then
-										v[3] = not v[3]
-										if v[3] == true then
-											local Text = Section.CurrentlySelectedObject.Text
-											Section.CurrentlySelectedObject.Text = Text:gsub("%[OFF%]", "")
-											Section.CurrentlySelectedObject.Text = Section.CurrentlySelectedObject.Text .. "[ON]"
-										else
-											local Text = Section.CurrentlySelectedObject.Text
-											Section.CurrentlySelectedObject.Text = Text:gsub("%[ON%]", "")
-											Section.CurrentlySelectedObject.Text = Section.CurrentlySelectedObject.Text .. "[OFF]"
-										end
-										v[2](v[3])
-										IsToggle = true
+								Section.ButtonNumber += 1
+								Section.CurrentlySelectedObject = Section.IObjects[Section.ButtonNumber][1]
+								if Section.CurrentlySelectedObject then
+									if Section.CurrentlySelectedObject:IsA("TextButton") then
+										Section.CurrentlySelectedObject.Text = "> " .. Section.CurrentlySelectedObject.Text
 									end
 								end
 							end
-							if not IsToggle then
-								for i,v in pairs(Section.IObjects) do
-									if v[1] and v[2] then
-										if v[1] == Section.CurrentlySelectedObject then
+						end
+						if Input.KeyCode == Enum.KeyCode.Up or Input.KeyCode == Enum.KeyCode.RightBracket then
+							if Section.ButtonNumber-1 > 0 then
+								if Section.CurrentlySelectedObject then
+									if Section.CurrentlySelectedObject:IsA("TextButton") then
+										local Text = Section.CurrentlySelectedObject.Text
+										Section.CurrentlySelectedObject.Text = Text:gsub("> ", "")
+									end
+								end
+								Section.ButtonNumber -= 1
+								Section.CurrentlySelectedObject = Section.IObjects[Section.ButtonNumber][1]
+								if Section.CurrentlySelectedObject then
+									if Section.CurrentlySelectedObject:IsA("TextButton") then
+										Section.CurrentlySelectedObject.Text = "> " .. Section.CurrentlySelectedObject.Text
+									end
+								end
+							end
+						end
+					end
+					if Input.KeyCode == Enum.KeyCode.Return then
+						if Section.CurrentlySelectedObject then
+							if SectionIOBject and SectionIOBject[4] and tostring(SectionIOBject[4]) == "Keybind" then
+								if Section.KeybindBusy then else
+									Section.KeybindBusy = true
+								end
+							else
+								if Section.Focused then
+									for i,v in pairs(Section.IObjects) do
+										if v == Section.CurrentlySelectedObject then
 											v[2]()
 										end
 									end
+									Section.Focused = false
+								else
+									local IsToggle = false
+									for i,v in pairs(Section.Toggles) do
+										if v[1] == Section.CurrentlySelectedObject then
+											if v[2] then
+												v[3] = not v[3]
+												if v[3] == true then
+													local Text = Section.CurrentlySelectedObject.Text
+													Section.CurrentlySelectedObject.Text = Text:gsub("%[OFF%]", "")
+													Section.CurrentlySelectedObject.Text = Section.CurrentlySelectedObject.Text .. "[ON]"
+												else
+													local Text = Section.CurrentlySelectedObject.Text
+													Section.CurrentlySelectedObject.Text = Text:gsub("%[ON%]", "")
+													Section.CurrentlySelectedObject.Text = Section.CurrentlySelectedObject.Text .. "[OFF]"
+												end
+												v[2](v[3])
+												IsToggle = true
+											end
+										end
+									end
+									if not IsToggle then
+										for i,v in pairs(Section.IObjects) do
+											if v[1] and v[2] then
+												if v[1] == Section.CurrentlySelectedObject then
+													v[2]()
+												end
+											end
+										end
+									end
+								end
+							end
+						end
+					end
+					if Input.KeyCode == Enum.KeyCode.Backspace and not Section.Focused then
+						lib.Focused = false
+						for i,v in pairs(Section.Objects) do
+							v.Visible = false
+						end
+					end
+					if Section.Focused then
+						for i,v in pairs(Section.IObjects) do
+							if v[1] == Section.CurrentlySelectedObject then
+								if v[2] and v[3] then
+									if tonumber(v[3]) then else
+										local WhitelistedChars = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890"
+										if Input.KeyCode == Enum.KeyCode.Backspace then
+											local TextLength = tostring(v[3]):len()
+											local OriginalText = tostring(v[3])
+											v[3] = ""
+											for i = 1,TextLength,1 do
+												if i == TextLength then else
+													v[3] = tostring(v[3] .. OriginalText:sub(i,i))
+												end
+											end
+											v[2]()
+										else
+											for i = 1,WhitelistedChars:len(),1 do
+												if WhitelistedChars:sub(i,i) == Input.KeyCode.Name then
+													v[3] = tostring(tostring(v[3]) .. Input.KeyCode.Name:lower())
+													v[2]()
+													break
+												end
+											end
+										end
+									end
+								end
+							end
+						end
+					else
+						for i,v in pairs(lib.KeyBinds) do
+							if v[1] and v[2] then
+								if v[1] == Input.KeyCode then
+									v[2](true)
 								end
 							end
 						end
 					end
 				end
-				if Input.KeyCode == Enum.KeyCode.Backspace and not Section.Focused then
-					lib.Focused = false
-					for i,v in pairs(Section.Objects) do
-						v.Visible = false
+			end
+		end
+	end)
+	UserInputService.InputEnded:Connect(function(Input)
+		if Input.UserInputType == Enum.UserInputType.MouseButton1 then
+			if Section.KeybindBusy and Section.CurrentlySelectedObject then else
+				for i,v in pairs(lib.KeyBinds) do
+					if v[1] == "LMB" then
+						v[2](false)
 					end
 				end
-				if Section.Focused then
-					for i,v in pairs(Section.IObjects) do
-						if v[1] == Section.CurrentlySelectedObject then
-							if v[2] and v[3] then
-								if tonumber(v[3]) then else
-									local WhitelistedChars = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890"
-									if Input.KeyCode == Enum.KeyCode.Backspace then
-										local TextLength = tostring(v[3]):len()
-										local OriginalText = tostring(v[3])
-										v[3] = ""
-										for i = 1,TextLength,1 do
-											if i == TextLength then else
-												v[3] = tostring(v[3] .. OriginalText:sub(i,i))
-											end
-										end
-										v[2]()
-									else
-										for i = 1,WhitelistedChars:len(),1 do
-											if WhitelistedChars:sub(i,i) == Input.KeyCode.Name then
-												v[3] = tostring(tostring(v[3]) .. Input.KeyCode.Name:lower())
-												v[2]()
-												break
-											end
-										end
-									end
+			end
+		end
+		if Input.UserInputType == Enum.UserInputType.MouseButton2 then
+			if Section.KeybindBusy and Section.CurrentlySelectedObject then else
+				for i,v in pairs(lib.KeyBinds) do
+					if v[1] == "RMB" then
+						v[2](false)
+					end
+				end
+			end
+		end
+		if Input.UserInputType == Enum.UserInputType.MouseButton3 then
+			if Section.KeybindBusy and Section.CurrentlySelectedObject then else
+				for i,v in pairs(lib.KeyBinds) do
+					if v[1] == "MMB" then
+						v[2](false)
+					end
+				end
+			end
+		end
+		if Input.UserInputType == Enum.UserInputType.Keyboard and lib.CurrentlySelectedSection == SectionButton and SectionButton.Visible == true and Section.CurrentlySelectedObject and lib.Focused == true and lib.MainGUIInstance.Visible == true then
+			if #Section.IObjects > 0 then
+				local SectionIOBject
+				for i,v in pairs(Section.IObjects) do
+					if v[1] == Section.CurrentlySelectedObject and v[2] and v[3] then
+						SectionIOBject = v
+					end
+				end
+				if SectionIOBject then
+					if not Section.Focused then
+						for i,v in pairs(lib.KeyBinds) do
+							if v[1] and v[2] then
+								if v[1] == Input.KeyCode then
+									v[2](false)
 								end
 							end
 						end
